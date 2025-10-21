311e5a75e6b1c7484366e6256818ae591e1f6d7e
thejuvies <125755806+thejuvies@users.noreply.github.com>
2025-10-21 11:13:41 +0000

    copying functionality added

--- app/card.tsx
+++ app/card.tsx
@@ -1,14 +1,16 @@
-import { TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { TouchableWithoutFeedback, Keyboard, StyleSheet, ScrollView } from 'react-native';
 import { ThemedView } from '@/components/themed-view';
 import { ThemedText } from '@/components/themed-text';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
+import * as Clipboard from "expo-clipboard";
+import { Alert } from "react-native";
 
 export default function CardModalScreen() {
-  
+
   let [title, setTitle] = useState('No Title');
   let [content, setContent] = useState('No Content');
-  useEffect(()=>{
+  useEffect(() => {
     const load = async () => {
       let params = (await AsyncStorage.getItem('displayItem'));
       params = JSON.parse(params);
@@ -16,19 +18,35 @@ export default function CardModalScreen() {
       setContent(params?.content || 'No Content');
     };
     load();
-  },[])
+  }, [])
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <ThemedView style={{ flex: 1, padding: 20, alignItems: 'center' }}>
-        <ThemedText style={{ fontSize: 22, fontWeight: 'bold', marginBottom: 20 }}>
-          {title}
-        </ThemedText>
-        <ThemedText style={{ fontSize: 18 }}>{content}</ThemedText>
+      <ScrollView
+      style={{ flex: 1 }}
+      contentContainerStyle={{ padding: 20 }}
+    >
+        <ThemedView style={{ flex: 1, padding: 20, alignItems: 'center' }}>
+        <ThemedText onPress={()=>{copyItem(title)}} style={styles.card}>{title}</ThemedText>
+        <ThemedText onPress={()=>{copyItem(content)}} style={styles.card}>{content}</ThemedText>
       </ThemedView>
+      </ScrollView>
     </TouchableWithoutFeedback>
   );
 }
 
-// async function loadData() {
-  
-// }
+const styles = StyleSheet.create({
+  card: {
+    fontSize: 18,
+    marginBottom: 20,
+    backgroundColor: '#1e1e1eff',
+    width: '100%',
+    padding: 5,
+    borderRadius: 7,
+    textAlign: 'center'
+  }
+})
+
+async function copyItem(data: string) {
+  await Clipboard.setStringAsync(data);
+  Alert.alert("Text Copied");
+}
